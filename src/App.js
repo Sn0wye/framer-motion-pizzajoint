ab10e8822fb364a13559b78fb7a3e23d036cbc2e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-04-16 18:55:06 -0300

    base transitions done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Base from "./components/Base";
 import Toppings from "./components/Toppings";
 import Order from "./components/Order";
+import { AnimatePresence } from "framer-motion";
+import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const location = useLocation();
+
   const [pizza, setPizza] = useState({ base: "", toppings: [] });
 
   const addBase = base => {
@@ -26,18 +32,27 @@ function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/base"
-          element={<Base addBase={addBase} pizza={pizza} />}
-        />
-        <Route
-          path="/toppings"
-          element={<Toppings addTopping={addTopping} pizza={pizza} />}
-        />
-        <Route path="/order" element={<Order pizza={pizza} />} />
-      </Routes>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => setShowModal(false)}
+      >
+        <Routes location={location} key={location.key}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/base"
+            element={<Base addBase={addBase} pizza={pizza} />}
+          />
+          <Route
+            path="/toppings"
+            element={<Toppings addTopping={addTopping} pizza={pizza} />}
+          />
+          <Route
+            path="/order"
+            element={<Order pizza={pizza} setShowModal={setShowModal} />}
+          />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
